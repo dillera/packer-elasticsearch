a2e3a804151f93f69330742a46eb8f9564b9183b
Michel Boudreau <michelb@pageuppeople.com>
2016-09-13 16:12:58 +1000

    making ssh timeout to be higher in case instance is taking a while to build, fixing default scripts to append to file and not wipe

--- scripts/default.sh
+++ scripts/default.sh
@@ -4,9 +4,9 @@ set -e
 
 sudo yum -y update
 
-sudo echo "elasticsearch soft nofile 128000" > /etc/security/limits.conf
-sudo echo "elasticsearch hard nofile 128000" > /etc/security/limits.conf
-sudo echo "root soft nofile 128000" > /etc/security/limits.conf
-sudo echo "root hard nofile 128000" > /etc/security/limits.conf
+sudo echo "elasticsearch soft nofile 128000" >> /etc/security/limits.conf
+sudo echo "elasticsearch hard nofile 128000" >> /etc/security/limits.conf
+sudo echo "root soft nofile 128000" >> /etc/security/limits.conf
+sudo echo "root hard nofile 128000" >> /etc/security/limits.conf
 
-sudo echo "fs.file-max = 500000" > /etc/sysctl.conf
+sudo echo "fs.file-max = 500000" >> /etc/sysctl.conf
