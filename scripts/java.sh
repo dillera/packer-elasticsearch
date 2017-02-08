6f92813378b1a64ca63ac1da64aa0ae933fe1d7e
Colin Scott <colin@abstractcode.com>
2017-02-08 15:08:46 +1100

    Refresh versions

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -eux
 
 set -e
-
-curl -L -o /tmp/jdk-8.rpm -b "oraclelicense=accept-securebackup-cookie"  "http://download.oracle.com/otn-pub/java/jdk/8u91-b14/jdk-8u91-linux-x64.rpm"
+curl -L -o /tmp/jdk-8.rpm -b "oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.rpm"
 yum localinstall -y /tmp/jdk-8.rpm
 
 echo "Check installation"
